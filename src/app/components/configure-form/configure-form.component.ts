e48c748614256fa0753ac37389e14bec18b2de89
Anirban Kar <anirban.kar@locusnine.com>
2022-05-26 12:14:35 +0530

    doc(configure-form): add docs for methods

--- src/app/components/configure-form/configure-form.component.ts
+++ src/app/components/configure-form/configure-form.component.ts
@@ -28,6 +28,9 @@ export class ConfigureFormComponent implements OnInit {
     this.initializeBuilderForm()
   }
 
+  /**
+   * Method to open modal
+   */
   open() {
     this.modalService.open(this.modal, { 
       ariaLabelledBy: 'modal-basic-title',
@@ -35,6 +38,9 @@ export class ConfigureFormComponent implements OnInit {
     })
   }
 
+  /**
+   * Method to initialize builder form
+   */
   initializeBuilderForm() {
     this.builderForm = new FormGroup({
       questionType: this.formBuilder.control('Textbox', Validators.required),
@@ -44,6 +50,9 @@ export class ConfigureFormComponent implements OnInit {
     this.addCheckboxOptionsField()
   }
 
+  /**
+   * Method to add checkbox options field to builder form if checkbox question type is selected
+   */
   addCheckboxOptionsField() {
     if (this.builderForm.controls['questionType']?.value === 'Checkbox') {
       this.builderForm.addControl('checkboxOptions', this.formBuilder.array([
@@ -56,26 +65,43 @@ export class ConfigureFormComponent implements OnInit {
     }
   }
 
+  /**
+   * Method to get checkbox options from builder form as a FormArray
+   */
   get checkboxOptions() { 
     return this.builderForm.get('checkboxOptions') as FormArray 
   }
 
+  /**
+   * Method to remove checkbox answer option from form array by index
+   * @param index 
+   */
   removeCheckboxOption(index: number) {
     const checkboxOptions = this.builderForm.get('checkboxOptions') as FormArray
     checkboxOptions.removeAt(index)
   }
 
+  /**
+   * Method to add checkbox option answer to form array in builder form
+   */
   addCheckboxOption() {
     const checkboxOptions = this.builderForm.get('checkboxOptions') as FormArray
     checkboxOptions.push(this.formBuilder.control(null, Validators.required))
   }
 
+  /**
+   * Method to submit builder form data
+   */
   submit() {
     this.buildForm(this.builderForm.value)
     this.initializeBuilderForm()
     this.modalService.dismissAll()
   }
 
+  /**
+   * Method to build form from builder form
+   * @param formData 
+   */
   buildForm(formData: ICustomFormControl) {
     if (formData['questionType'] === 'Textbox') {
       this.form.addControl(
@@ -92,6 +118,11 @@ export class ConfigureFormComponent implements OnInit {
     this.formDataList.push(formData)
   }
 
+  /**
+   * Method to build checkbox controls from the submitted data in builder form
+   * @param options 
+   * @returns 
+   */
   buildCheckboxControls(options: any[]) {
     const arr = options.map(value => {
       return this.formBuilder.control(value.selected);
@@ -99,10 +130,20 @@ export class ConfigureFormComponent implements OnInit {
     return this.formBuilder.array(arr);
   }
 
+  /**
+   * Method to create an unique control name for form
+   * @param key 
+   * @param index 
+   * @returns 
+   */
   preProcessControlName(key: string, index: number) {
     return (key.replace(/[^a-zA-Z0-9]/g, "") + '-' +index).toLowerCase()
   }
 
+  /**
+   * Method to map data between form data list and form submitted to create results array and navigate to answers page
+   * @param submittedFormData 
+   */
   reviewAnswers(submittedFormData: any) {
     const result: any[] = []
     let i = 0
